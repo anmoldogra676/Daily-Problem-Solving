978174dedea16e377bf2ffb40e264bb7617b48a7
Anmol Dogra <56497834+anmoldogra676@users.noreply.github.com>
2022-06-22 22:01:30 +0530

    Time: 23 ms (11.49%), Space: 44.7 MB (46.73%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,14 +1,55 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer>pq = new PriorityQueue<>();
         
-        for(int i=0;i<nums.length ;i++){
-             pq.add(nums[i]);
-            if(pq.size()>k)
-             pq.remove();
-               
-            
-        }
-        return pq.peek();
+        // T.c : O(nlogk)
+        // PriorityQueue<Integer>pq = new PriorityQueue<>(); 
+        // for(int i=0;i<nums.length ;i++){
+        //      pq.add(nums[i]);
+        //      if(pq.size()>k)
+        //      pq.remove();
+        // }
+        // return pq.peek();
+        // based upon quick Select
+         k= nums.length-k;
+        return helper(nums, k, 0,nums.length-1);
     }
+    public int helper(int[]arr, int k ,int st, int end){
+      
+        int partitionIndex= helper2(arr, st ,end); // is idx k baad sb bde 
+        if(partitionIndex==k){
+            return arr[k];
+        }else if(partitionIndex>k){
+            return helper(arr, k, st, partitionIndex-1);
+        }else{
+             return helper(arr, k, partitionIndex+1, end);
+        }
+    
+    }
+    
+    public int helper2(int[]arr, int st, int end){
+        // Two pointer i, j, 
+        // 0 to j-1 --> smaller
+        // j to i-1 -->larger
+        // i to end unknown
+        int pivot = arr[end];
+        int i=st;
+        int j=st;
+        while(i<= end){
+            if(arr[i]>pivot){
+                i++;
+            }else{
+                swap(arr, i ,j);
+                i++;
+                j++;
+            }
+        }
+        
+        return j-1;
+    }
+    public void swap(int []arr, int i, int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    
 }
