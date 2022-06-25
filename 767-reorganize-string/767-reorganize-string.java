a3d3811cc8939564e047d8527963571b73ff3507
Anmol Dogra <56497834+anmoldogra676@users.noreply.github.com>
2022-06-26 01:59:25 +0530

    Time: 7 ms (40.45%), Space: 42.5 MB (34.82%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.java
@@ -0,0 +1,53 @@
+class Solution {
+     class pair{
+        char ch;
+        int val;
+        pair(char ch, int val){
+            this.ch =ch;
+            this.val=val;
+        }
+        pair(){
+            
+        }
+    }
+    
+   
+    public String reorganizeString(String s) {
+        StringBuilder ans = new StringBuilder();
+        int[] ch1 = new int[26];
+        for(char ch2 : s.toCharArray()){
+            ch1[ch2-'a']++;
+        }
+        PriorityQueue<pair>pq =new PriorityQueue<>((a,b)->{
+            return b.val-a.val;
+        });
+        for(int i=0;i<26;i++){
+            if(ch1[i]!=0){
+                pq.add(new pair( (char)('a'+i), ch1[i])); // character and kitni baar appear hua hai
+            }
+        }
+        pair save=null;
+        while(pq.size()>0){
+    
+            pair p =pq.poll();
+            char ch = p.ch;
+            int val = p.val;
+            if(save!=null && ch==save.ch)return "";
+            val--;
+            ans.append(ch);
+            if(save!=null && ch!=save.ch){
+                pq.add(save);
+            }
+            if(ans.length()>0 && ans.charAt(ans.length()-1)==ch && val>0){
+                pair pp = new pair(ch , val);
+                save = pp;
+            }else{
+                save =null;
+            }
+        
+            
+        }
+        if(ans.length()!=s.length())return"";
+        return ans.toString();
+    }
+}
