8fc36922d3dbee6d2d2195177a7c02c2f1c1efd4
Anmol Dogra <56497834+anmoldogra676@users.noreply.github.com>
2022-07-03 04:20:21 +0530

    Time: 74 ms (14.31%), Space: 54 MB (21.39%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,11 +1,26 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
+    public int coinChange(int[] arr, int amount) {
         // 
-        int dp[][] = new int[coins.length][amount+1];
-        for(int []a: dp)Arrays.fill(a, -1);
-        int val= f(coins.length-1, amount, coins,dp);
-        if(val>= 100000)return -1;
-        return val;
+        int dp[][] = new int[arr.length][amount+1];
+        int n=arr.length;
+        // for(int []a: dp)Arrays.fill(a, -1);
+        // int val= f(coins.length-1, amount, coins,dp);
+        // if(val>= 100000)return -1;
+        for(int i=0;i<n;i++)dp[i][0]=0;
+        for(int i=1;i<dp[0].length ;i++){
+            if(i%arr[0]==0)dp[0][i]= i/arr[0];
+            else dp[0][i]=100000;
+        }
+        for(int i=1;i<arr.length;i++){
+            for(int t=1;t<dp[0].length ;t++){
+                int ta =100000;
+        if(t-arr[i]>=0)
+        ta = 1+ f(i, t-arr[i], arr,dp);
+        int nt = 0+ f(i-1, t,arr,dp);
+           dp[i][t]=Math.min(nt, ta);
+            }
+        }
+        return dp[arr.length-1][amount]>=100000?-1:dp[arr.length-1][amount];
     }
     public int f(int i, int t, int []arr, int[][]dp){
         if(i==0){
