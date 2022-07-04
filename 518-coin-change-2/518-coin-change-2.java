de2fd5588efced3a37d8b454dee03aded44ad189
Anmol Dogra <56497834+anmoldogra676@users.noreply.github.com>
2022-07-04 11:28:27 +0530

    Time: 7 ms (57.34%), Space: 49.3 MB (46.88%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,11 +1,23 @@
 class Solution {
-    public int change(int amount, int[] coins) {
+    public int change(int amount, int[] arr) {
         // f( amount , coins, n-1) --> coins array from starting 0 to n-1 kitne combination bn skte amount k
-        int n=coins.length;
+        int n=arr.length;
         int [][]dp = new int[n][amount+1];
-        for(int[]a:dp)Arrays.fill(a, -1);
-        return helper(amount, coins,n-1,dp);
-        
+        // for(int[]a:dp)Arrays.fill(a, -1);
+        // return helper(amount, coins,n-1,dp);--->Memoization
+        // tabulation-->
+        // base case
+        for(int i=0;i<=amount;i++){
+            if(i%arr[0]==0)dp[0][i]=1;
+        }
+        // traverse
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                dp[i][j]=dp[i-1][j];
+                if(j-arr[i]>=0)dp[i][j]+=dp[i][j-arr[i]];
+            }
+        }
+        return dp[n-1][amount];
     }
     public int helper(int a, int []arr, int n, int[][]dp){
         if(n==0){
