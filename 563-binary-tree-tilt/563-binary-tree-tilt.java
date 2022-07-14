e59a87ee7223456256e2607ca5aeb70baad5286f
Anmol Dogra <56497834+anmoldogra676@users.noreply.github.com>
2022-07-14 17:40:49 +0530

    Time: 1 ms (78.59%), Space: 42.8 MB (84.57%) - LeetHub

--- 563-binary-tree-tilt/563-binary-tree-tilt.java
+++ 563-binary-tree-tilt/563-binary-tree-tilt.java
@@ -13,26 +13,57 @@
  *     }
  * }
  */
-class Solution {
+class Solution { // 0(n2)
+    class pair{
+        int tilt;
+        int sum;
+        pair(int tilt, int sum){
+            this.tilt=tilt;
+            this.sum=sum;
+        }
+        pair(){
+            
+        }
+    }
     public int findTilt(TreeNode root) {
         // dfs recursion
-        return helper(root);
+        pair p =helper(root);
+        return p.tilt;
     }
-    public int helper(TreeNode root){
-        if(root==null)return 0;
-        if(root.left==null && root.right==null)return 0;
-        int lefttilt = helper(root.left);
-        int righttilt = helper(root.right);
-        int lsum = sum(root.left);
-        int rsum = sum(root.right);
-        return lefttilt +righttilt +Math.abs(lsum-rsum);
     
+    public pair helper(TreeNode root){
+        if(root==null){
+            return new pair(0,0);
+        }
+        if(root.left==null && root.right==null){
+            return new pair(0,root.val);
+        }
+        pair l = helper(root.left);
+        pair r = helper(root.right);
+        pair m = new pair();
+        // tilt , sum
+        m.sum = l.sum+r.sum+ root.val;
+        m.tilt = l.tilt +r.tilt + Math.abs(l.sum-r.sum);
+        return m;
+        
     }
-    public int sum(TreeNode root){
-        if(root==null)return 0;
-        int ans=0;
-        ans+=root.val;
-        ans+=sum(root.left)+sum(root.right);
-        return ans;
-    }
+    
+    
+//     public int helper(TreeNode root){
+//         if(root==null)return 0;
+//         if(root.left==null && root.right==null)return 0;
+//         int lefttilt = helper(root.left);
+//         int righttilt = helper(root.right);
+//         int lsum = sum(root.left);
+//         int rsum = sum(root.right);
+//         return lefttilt +righttilt +Math.abs(lsum-rsum);
+    
+//     }
+//     public int sum(TreeNode root){  // o(n)
+//         if(root==null)return 0;
+//         int ans=0;
+//         ans+=root.val;
+//         ans+=sum(root.left)+sum(root.right);
+//         return ans;
+//     }
 }
