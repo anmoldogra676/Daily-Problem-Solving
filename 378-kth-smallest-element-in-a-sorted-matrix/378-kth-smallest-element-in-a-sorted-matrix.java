ff5a69b5299de4eeee36e8463ba3854423a29014
Anmol Dogra <56497834+anmoldogra676@users.noreply.github.com>
2022-08-02 05:46:12 +0530

    Time: 18 ms (61.44%), Space: 56.3 MB (72.83%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,20 +1,20 @@
 class Solution {
-    public int kthSmallest(int[][] matrix, int k) {
-        int m = matrix.length ,n = matrix[0].length;
-        int len = m*n;
-        int [] arr = new int[len];
-        int var =0;
-        for(int i=0; i<m ; i++){
-            for(int j=0;j<n;j++){
-                arr[var++] = matrix[i][j];
+    public int kthSmallest(int[][] arr, int k) {
+        PriorityQueue<Integer>pq = new PriorityQueue<>((a,b)->{
+            return b-a;
+        });
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(pq.size()<k){
+                    pq.add(arr[i][j]);
+                }else{
+                    if(pq.peek()>=arr[i][j]){
+                        pq.poll();
+                        pq.add(arr[i][j]);
+                    }
+                }
             }
         }
-        Arrays.sort(arr);
-        for(int i=0 ;i <arr.length ;i++){
-            if(k==i+1){
-                return arr[i];
-            }
-        }
-        return -1;
+        return pq.peek();
     }
 }
