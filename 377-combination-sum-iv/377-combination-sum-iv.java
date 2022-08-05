13a0603932cc25a92737fa684da54682b39870d1
Anmol Dogra <56497834+anmoldogra676@users.noreply.github.com>
2022-08-05 17:18:33 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (77.40%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,7 +1,7 @@
 class Solution {
     public int combinationSum4(int[] arr, int t) {
         dp= new int[t+1];
-        Arrays.fill(dp, -1);
+        Arrays.fill(dp, -2);
         return helper(arr, t,dp);
     }
     int[]dp;
@@ -9,7 +9,7 @@ class Solution {
         if(t<0 )return 0;
         if(t==0)return 1;
         int ans=0;
-        if(dp[t]!=-1) return dp[t];
+        if(dp[t]!=-2) return dp[t];
         for(int i=0;i<arr.length;i++){
             ans+= helper(arr, t-arr[i],dp);
         }
