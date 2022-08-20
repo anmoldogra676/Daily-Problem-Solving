c7e40133cd1b6134d340df426516a76bd9dc73ae
Anmol Dogra <56497834+anmoldogra676@users.noreply.github.com>
2022-08-21 03:35:05 +0530

    Time: 118 ms (74.05%), Space: 138.4 MB (5.41%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -1,87 +1,56 @@
+// Fenwick Tree
 class NumArray {
     
-    class Node{
-        int start;
-        int end;
-        Node left;
-        Node right;
-        int sum;
-        Node(int start, int end){
-            this.start=start;
-            this.end= end;
-            this.left=null;
-            this.right=null;
-            this.sum=0;
+    class ftree{
+        int []ft;
+        ftree(int []nums){
+            ft= new int[nums.length+1];
+            for(int i=0;i<or.length;i++){
+                int pos = i+1;
+                update(pos,nums[i]);
+            }
         }
+        private int rsb(int x){
+            return x&-x;
+        }
+        int getsum (int pos){
+            int val=0;
+            while(pos>0){
+                val+=ft[pos];
+                pos-=rsb(pos);
+            }
+            return val;
+        }
+        void update(int pos, int delta){
+            while(pos<ft.length){
+                ft[pos]+=delta;
+                pos = pos+rsb(pos);
+            }
+            
+        }
+        
     }
-    Node root;
+    int [] or;
+    ftree node;
     public NumArray(int[] nums) {
-        // make root
-        root = build(nums, 0 , nums.length-1);
-        // Tree Bn gya segment Tree
-    }
-    // Buidling The Tree--> 
-    public Node build(int[]arr, int i , int j){
-        if(i>j)return null;
-        if(i==j){
-            Node nn =new Node(i,j);
-            nn.sum= arr[i];
-            return nn;
-        } 
-        // aisa nhi hai toh recursive tree bnao
-        Node nn = new Node(i,j);
-        int mid = (i+j)/2;
-        nn.left= build(arr, i, mid);
-        nn.right= build(arr, mid+1, j);
-        nn.sum= nn.left.sum+nn.right.sum;
-        return nn;
-    }
-    
-    public void update(int index, int val) {
-        // update value toh simply faith
-        helper(root, index, val);
-    }
-    // update the value as well as tree result
-    public void helper(Node root, int pos, int val){
-        // found leaf node that we want to update
-        if(root.start== root.end){
-            root.sum= val;
-            return;
-        }
-        // check mid --> for deciding which side to go
-        int mid = root.start +(root.end-root.start)/2;
-        if(pos<=mid){
-            // left part
-            helper(root.left, pos, val);
-        }else{
-            // right part
-            helper(root.right, pos, val);
-        }
-        root.sum = root.left.sum+root.right.sum;
+        or= nums;
+        node = new ftree(nums);
         
     }
     
-    // we have to find sum between left and right Range
-    public int sumRange(int left, int right) {
-        return h(root, left, right);
+    public void update(int index, int val) {
+        int delta = val-or[index];
+        node.update(index+1,delta);
+        or[index]=val;
     }
     
-    public int h(Node root, int l, int r){
-        // sum calculate krna hai between l, r
-        if(root.start==l && root.end==r){
-            // same mil gya 
-            return root.sum;
-        }
-        int mid = root.start + (root.end-root.start)/2;
-        if(r<=mid){
-            // left part m 
-            return h(root.left, l, r);
-        }else if(l>mid){
-            // right part m jao
-            return h(root.right, l, r);
-        }else
-        // consider both
-        return h(root.left, l, mid) +h(root.right, mid+1, r);
+    public int sumRange(int left, int right) {
+        // now we have to calculate the sum
+        int lpos = left+1;
+        int rpos = right+1;
+        int leftSum = node.getsum(lpos-1);
+        int rightSum = node.getsum(rpos);
+        return rightSum-leftSum;
     }
 }
 
